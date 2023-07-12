f657e425a7d6214daa76cc98f1556161282b79ad
김성재 <103095432+seongjae6751@users.noreply.github.com>
2023-07-12 23:14:37 +0900

    section4_1

--- Assets/Assets/Scripts/Rock.cs
+++ Assets/Assets/Scripts/Rock.cs
@@ -13,20 +13,26 @@ public class Rock : MonoBehaviour
     [SerializeField]
     private SphereCollider col; // 구체 콜라이더
 
+    // 필요한 게임 오브젝트
     [SerializeField]
     private GameObject go_rock; // 일반 바위
-
     [SerializeField]
     private GameObject go_debris; // 깨진 바위
-
     [SerializeField]
     private GameObject go_effect_prefabs; // 채굴 이펙트
+    [SerializeField]
+    private GameObject go_rock_item_prefab; // 돌멩이 아이템
+
+    // 돌맹이 아이템 등장 개수.
+    [SerializeField]
+    private int count;
 
     // 필요한 사운드 이름
     [SerializeField]
     private string strike_Sound;
     [SerializeField]
     private string destroy_Sound;
+    
 
     public void Mining()
     {
@@ -41,7 +47,14 @@ public class Rock : MonoBehaviour
     private void Destruction()
     {
         SoundManager.instance.PlaySE(destroy_Sound);
+
         col.enabled = false;
+
+        for (int i = 0; i <= count; i++)
+        {
+            Instantiate(go_rock_item_prefab, go_rock.transform.position, Quaternion.identity);
+        }
+
         Destroy(go_rock);
 
         go_debris.SetActive(true);
